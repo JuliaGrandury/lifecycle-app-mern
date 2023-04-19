24bad67cab560a28cd8b92f078ca5a09e0fa9411
juliagrandury <juliagrandury@gmail.com>
2023-04-19 15:51:58 -0400

    Last commit before first deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -13,6 +14,14 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/items', require('./routes/itemRoutes'))
 app.use('/api/v1/users', require('./routes/userRoutes'));
+
+// serve the frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set environment to production'))
+}
 
 app.use(errorHandler);
 
