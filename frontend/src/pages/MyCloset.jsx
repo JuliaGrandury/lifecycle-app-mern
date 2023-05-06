a70f603ecb20613c3ae713019540abe88248e5fe
juliagrandury <juliagrandury@gmail.com>
2023-05-05 23:27:35 -0400

    Toggling and storing closet search

--- frontend/src/pages/MyCloset.jsx
+++ frontend/src/pages/MyCloset.jsx
@@ -1,100 +1,94 @@
-import { IoAddCircle, IoSearchCircle, IoFilterCircle } from "react-icons/io5";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { getItems, reset } from "../features/items/itemSlice";
-import ItemForm from "../components/ItemForm";
-import ItemCompSmall from "../components/ItemCompSmall";
-import FilterBar from "../components/FilterBar";
-import Spinner from "../components/Spinner";
-import styles from "./Closets.module.css";
-import { toast } from "react-toastify";
-import squiggle_arrow from "../assets/squiggle_arrow.png";
+import { IoAddCircle, IoSearchCircle, IoFilterCircle } from "react-icons/io5"
+import { useState } from "react"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { getItems, reset } from "../features/items/itemSlice"
+import ItemForm from "../components/ItemForm"
+import ItemCompSmall from "../components/ItemCompSmall"
+import FilterBar from "../components/FilterBar"
+import Spinner from "../components/Spinner"
+import styles from "./Closets.module.css"
+import { toast } from "react-toastify"
+import squiggle_arrow from "../assets/squiggle_arrow.png"
 
 const MyCloset = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const { user } = useSelector((state) => state.auth);
-  const { items, isLoading, isError, message } = useSelector(
-    (state) => state.items
-  );
-  const [showForm, setShowForm] = useState(false);
-  const [filterDropdown, setFilterDropdown] = useState(false);
-  const [searchActive, setSearchActive] = useState(false);
+  const { user } = useSelector((state) => state.auth)
+  const { items, isLoading, isError, message } = useSelector((state) => state.items)
+  const [showForm, setShowForm] = useState(false)
+  const [filterDropdown, setFilterDropdown] = useState(false)
+  const [searchActive, setSearchActive] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
 
   useEffect(() => {
     if (isError) {
-      console.log(message);
+      console.log(message)
     }
 
     if (!user) {
-      navigate("/authentication");
+      navigate("/authentication")
     }
 
-    dispatch(getItems());
+    dispatch(getItems())
 
     // return () => {
     //   dispatch(reset())
     // }
-  }, [user, navigate, isError, message, dispatch]);
+  }, [user, navigate, isError, message, dispatch])
 
   if (isLoading) {
-    return <Spinner />;
+    return <Spinner />
   }
 
   const closeItemForm = () => {
-    setShowForm(false);
-  };
+    setShowForm(false)
+  }
 
   const handleFilter = () => {
-    toast(
-      "This feature is currently in development. Please check back at a later time."
-    );
-  };
+    toast("This feature is currently in development. Please check back at a later time.")
+  }
 
-  const handleSearch = () => {
-    setSearchActive(true);
-    
-  };
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value)
+  }
+
+  const submitSearch = (event) => {
+    event.preventDefault()
+    console.log(searchQuery)
+    // setSearchActive(false)
+  }
 
   return (
     <>
       <section className={styles.closet__heading}>
         <FilterBar />
         <ul className={styles.closet__actions__list}>
-          <li>
-            <button
-              className={styles.action__button}
-              onClick={() => handleFilter()}
-            >
-              <IoFilterCircle />
-            </button>
-          </li>
-
           {searchActive ? (
             <li>
-              <input type="text" name="searchquery" id="" />
+              <form onSubmit={submitSearch}>
+                <input type="text" value={searchQuery} onChange={handleSearch} />
+              </form>
             </li>
           ) : (
             <></>
           )}
-
           <li>
-            <button
-              className={styles.action__button}
-              onClick={() => handleSearch()}
-            >
+            <button className={styles.action__button} onClick={() => setSearchActive(!searchActive)}>
               <IoSearchCircle />
             </button>
           </li>
 
           <li>
-            <button
-              className={styles.action__button}
-              onClick={() => setShowForm(true)}
-            >
+            <button className={styles.action__button} onClick={() => handleFilter()}>
+              <IoFilterCircle />
+            </button>
+          </li>
+
+          <li>
+            <button className={styles.action__button} onClick={() => setShowForm(true)}>
               <IoAddCircle />
             </button>
           </li>
@@ -105,10 +99,7 @@ const MyCloset = () => {
 
       <section className={styles.items__container}>
         {items.length > 0 ? (
-          <div className={styles.item__grid}>
-            {items &&
-              items.map((item) => <ItemCompSmall key={item._id} item={item} />)}
-          </div>
+          <div className={styles.item__grid}>{items && items.map((item) => <ItemCompSmall key={item._id} item={item} />)}</div>
         ) : (
           <div
             style={{
@@ -117,21 +108,14 @@ const MyCloset = () => {
               flexWrap: "wrap",
               justifyContent: "right",
               alignItems: "center",
-            }}
-          >
-            <h3 style={{ color: "var(--primary-galactic)", fontSize: "25px" }}>
-              Your closet is empty. Add items here.
-            </h3>
-            <img
-              style={{ padding: "0px 60px 60px 30px" }}
-              src={squiggle_arrow}
-              alt=""
-            />
+            }}>
+            <h3 style={{ color: "var(--primary-galactic)", fontSize: "25px" }}>Your closet is empty. Add items here.</h3>
+            <img style={{ padding: "0px 60px 60px 30px" }} src={squiggle_arrow} alt="" />
           </div>
         )}
       </section>
     </>
-  );
-};
+  )
+}
 
-export default MyCloset;
+export default MyCloset
