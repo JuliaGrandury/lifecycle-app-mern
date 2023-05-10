eb99148777da012b88d5d213cd336bd7681d57f5
juliagrandury <juliagrandury@gmail.com>
2023-05-10 16:26:13 -0400

    Overlay addition on card expansion successful

--- frontend/src/components/ItemCompLarge.jsx
+++ frontend/src/components/ItemCompLarge.jsx
@@ -26,80 +26,84 @@ function ItemCompLarge({ item, onClose }) {
   }
 
   return (
-    <motion.div
-      transition={{ layout: { duration: 1, type: "spring" } }}
-      layout
-      style={{ borderRadius: "1rem" }}
-      className={isOpen ? `${styles.item__card} ${styles.expanded__card}` : `${styles.item__card}`}
-      onClick={() => setIsOpen(!isOpen)}>
-      <motion.div className={styles.item__header}>
-        <motion.h2 layout="position">{item.name}</motion.h2>
-        <div className={styles.item__details}>
-          <span>
-            Size {item.size} {item.brand ? `, ${item.brand}` : ""}
-          </span>
-          <span>
-            {item.color.map((color) => (
-              <ColorSphere color={color} key={`${color}${item._id}`} />
-            ))}
-          </span>
-        </div>
-      </motion.div>
-      <div className={styles.item__image}>
-        <img src={default_image} alt={item.name} />
-      </div>
-      {isOpen && (
-        <motion.div className={styles.item__moredetails} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-          <table>
-            <tbody>
-              <tr>
-                <td>Category</td>
-                <td>{item.category}</td>
-              </tr>
-              <tr>
-                <td>Subcategory</td>
-                <td>{item.subcategory}</td>
-              </tr>
-              <tr>
-                <td>Added On</td>
-                <td>{new Date(item.createdAt).toLocaleDateString("en-US")}</td>
-              </tr>
-              <tr>
-                <td>Times Worn</td>
-                <td>{item.datesWorn.length}</td>
-              </tr>
-              <tr>
-                <td>Value</td>
-                <td>{item.value === 100 ? 'Sentimental' : item.value}</td>
-              </tr>
-              <tr>
-                <td>Washing Instructions</td>
-                <td>{item.washInstructions}</td>
-              </tr>
-            </tbody>
-          </table>
-          {isOwner ? (
-            <div className={styles.largecard__actions}>
-              <button className="btn" id="editItem" onClick={() => handleEditItem(item.id)}>
-                Edit
-              </button>
-              <button className="btn" id="duplicateItem" onClick={() => handleDuplicateItem(item.id)}>
-                Duplicate
-              </button>
-              <button className="btn" id="deleteItem" onClick={() => handleDeleteItem(item._id)}>
-                Delete
-              </button>
-            </div>
-          ) : (
-            <div className={styles.largecard__actions}>
-              <button className="btn">Add to Favorites</button>
-              <button className="btn">Request to Borrow</button>
-              <button className="btn">Request to Swap</button>
-            </div>
-          )}
+    <>
+      {isOpen && <div className={styles.overlay}></div>}
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        layout
+        style={{ borderRadius: "1rem" }}
+        className={isOpen ? `${styles.item__card} ${styles.expanded__card}` : `${styles.item__card}`}
+        onClick={() => setIsOpen(!isOpen)}>
+        <motion.div className={styles.item__header}>
+          <motion.h2 layout="position">{item.name}</motion.h2>
+          <div className={styles.item__details}>
+            <span>
+              Size {item.size} {item.brand ? `, ${item.brand}` : ""}
+            </span>
+            <span>
+              {item.color.map((color) => (
+                <ColorSphere color={color} key={`${color}${item._id}`} />
+              ))}
+            </span>
+          </div>
         </motion.div>
-      )}
-    </motion.div>
+        <div className={styles.item__image}>
+          <img src={default_image} alt={item.name} />
+        </div>
+        {isOpen && (
+          <motion.div className={styles.item__moredetails} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Category</td>
+                  <td>{item.category}</td>
+                </tr>
+                <tr>
+                  <td>Subcategory</td>
+                  <td>{item.subcategory}</td>
+                </tr>
+                <tr>
+                  <td>Added On</td>
+                  <td>{new Date(item.createdAt).toLocaleDateString("en-US")}</td>
+                </tr>
+                <tr>
+                  <td>Times Worn</td>
+                  <td>{item.datesWorn.length}</td>
+                </tr>
+                <tr>
+                  <td>Value</td>
+                  <td>{item.value === 100 ? "Sentimental" : item.value}</td>
+                </tr>
+                <tr>
+                  <td>Washing Instructions</td>
+                  <td>{item.washInstructions}</td>
+                </tr>
+              </tbody>
+            </table>
+
+            {isOwner ? (
+              <div className={styles.largecard__actions}>
+                <button className="btn" id="editItem" onClick={() => handleEditItem(item.id)}>
+                  Edit
+                </button>
+                <button className="btn" id="duplicateItem" onClick={() => handleDuplicateItem(item.id)}>
+                  Duplicate
+                </button>
+                <button className="btn" id="deleteItem" onClick={() => handleDeleteItem(item._id)}>
+                  Delete
+                </button>
+              </div>
+            ) : (
+              <div className={styles.largecard__actions}>
+                <button className="btn">Add to Favorites</button>
+                <button className="btn">Request to Borrow</button>
+                <button className="btn">Request to Swap</button>
+              </div>
+            )}
+          </motion.div>
+        )}
+      </motion.div>
+    </>
   )
 }
 
